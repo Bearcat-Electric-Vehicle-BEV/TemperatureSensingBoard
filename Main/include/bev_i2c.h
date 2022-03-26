cfda9091e3bc98720545f0b957710d461e1fe9a9
Marshal Stewart <pontiacmarshal@gmail.com>
2022-03-25 20:14:21 -0400

    Removed unused macros

--- Main/include/bev_i2c.h
+++ Main/include/bev_i2c.h
@@ -23,14 +23,6 @@
  * are needed.
  */
 
-#define DISPLAY_PARAM_MIN 0
-#define DISPLAY_SPEED 1
-#define DISPLAY_BATTERY_TEMP 2
-#define DISPLAY_BATTERY_LIFE 3
-#define DISPLAY_RANGE_KM 4
-#define DISPLAY_RANGE_MINS 5
-#define DISPLAY_PARAM_MAX 6
-
 #define MIN_I2C_ADDR 0x1
 #define DISPLAY_I2C_ADDR 0x40
 #define MAX_I2C_ADDR 0x7F
